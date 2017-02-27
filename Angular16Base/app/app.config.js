cb847b0d8e65b21d584c702d64780d33fe0a4a3d
Laudelino Adao <lalau_aj@hotmail.com>
2017-02-27 19:51:56 -0300

    Base Angular 1.6 structure

---
+++ Angular16Base/app/app.config.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+
+angular.
+  module('app').
+  config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+
+        //$routeProvider.
+        //  when('/phones', {
+        //      template: '<phone-list></phone-list>'
+        //  }).
+        //  when('/phones/:phoneId', {
+        //      template: '<phone-detail></phone-detail>'
+        //  }).
+        //  otherwise('/phones');
+    }
+  ]);
